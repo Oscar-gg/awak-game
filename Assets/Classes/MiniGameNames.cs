f7753b10029902fa8dd6873b89cb574077883257
Oscar-gg <oscar.arreola.jr@gmail.com>
2024-06-10 23:14:43 -0600

    Unir juegos

--- Assets/Classes/MiniGameNames.cs
+++ Assets/Classes/MiniGameNames.cs
@@ -1,11 +1,11 @@
 public static class MiniGameNames
 {
     public const string MAZE = "Laberinto";
-    public const string ETHICS_BOSS = "Jefe - Ética";
+    public const string ETHICS_BOSS = "Jefe - Etica";
     public const string MEMORY_GAME = "Memorama";
-    public const string COMMUNICATIONS_BOSS = "Jefe - Comunicación";
+    public const string COMMUNICATIONS_BOSS = "Jefe - Comunicacion";
     public const string PLATAFORMS = "Plataforma";
     public const string SECURITY_BOSS = "Jefe - Seguridad";
-    public const string ASSOCIATION = "Asociación";
+    public const string ASSOCIATION = "Asociacion";
     public const string TEDI_BOSS = "Jefe - TEDI";
 }
--- Assets/Classes/SceneNames.cs
+++ Assets/Classes/SceneNames.cs
@@ -12,4 +12,6 @@ public static class SceneNames
     public const string BOSS_ETHICS = "BossSceneEtica";
     public const string BOSS_SECURITY = "BossSceneSeguridad";
     public const string BOSS_TEDI = "BossSceneTEDI";
+    public const string MAZE = "LabeScene";
+    
 }
